d648c7582434327faf6766b45c738d7421c528f7
Phillip Tularak <phillip.tularak@gmail.com>
2016-05-10 20:53:59 -0400

    Adds more HTML layout

--- assets/js/run.js
+++ assets/js/run.js
@@ -1,6 +1,5 @@
-// $('#nav').affix({
-    
-//     offset: {
-//         top: $('header').height()
-//     }
-// });	
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
